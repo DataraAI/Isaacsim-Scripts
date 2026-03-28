e82e66c89390768720116a4c0959ffbe123d052e
Aayush Dubey <aayush@dataraai.ai>
2026-03-27 20:08:47 -0700

    Adding movements for target cube, option to be invisible
    If the target cube is able to be translated across the environment then the robot arm can follow. It starts with one port location, then moves onto the next.
    
    Soon I'll be adding more locations and iterate through them all, this way it will give the appearance of the robot arm interacting with each port of the network switch.

--- baseline/current/baseline_franka_hello_world.py
+++ baseline/current/baseline_franka_hello_world.py
@@ -7,12 +7,102 @@ import omni.kit.commands
 import omni.usd
 
 from isaacsim.core.utils.stage import add_reference_to_stage
-from pxr import UsdGeom, Gf
+from pxr import UsdGeom, Gf, Sdf, Usd
 from isaacsim.core.prims import Articulation
 
 import numpy as np
 
+import carb
+
 USD_PATH = r"/home/advaith/Downloads/Assets/DigitalTwin/Assets/Datacenter/Facilities/Stages/Data_Hall/DataHall_Full_01.usd"
+# PORT_PRIM_PATH = (
+#     "/World/Datacenter/Network_Switches/SN4600C_CS2FC_02/msn4600_cs2fc_01/SN4600C_A_01/msn4600_cs2fc_base/"
+#     "SM4600_CS2FC_01/NetworkConnectors/pcb003636_idf_01/Connector_Quad_01/Connector_Pair_01/QSFP_DD_Connector_A_01"
+# )
+
+PORT_PRIM_PATH = "/World/Datacenter/Network_Switches/SN4600C_CS2FC_02/msn4600_cs2fc_01/SN4600C_A_01/msn4600_cs2fc_base/SM4600_CS2FC_01/NetworkConnectors/pcb003636_idf_01/Connector_Quad_01/Connector_Pair_01/QSFP_DD_Connector_A_01"
+PORT_PRIM_PATH_2 = "/World/Datacenter/Network_Switches/SN4600C_CS2FC_02/msn4600_cs2fc_01/SN4600C_A_01/msn4600_cs2fc_base/SM4600_CS2FC_01/NetworkConnectors/pcb003636_idf_01/Connector_Quad_01/Connector_Pair_01/QSFP_DD_Connector_A_02"
+
+
+# /World/Datacenter/Network_Switches/SN4600C_CS2FC_02/msn4600_cs2fc_01/SN4600C_A_01/msn4600_cs2fc_base/SM4600_CS2FC_01/NetworkConnectors/pcb003636_idf_01/Connector_Quad_01/Connector_Pair_02/QSFP_DD_Connector_A_02
+
+
+PORT_PRIM_PATH_LIST = [
+    PORT_PRIM_PATH,
+    PORT_PRIM_PATH_2
+]
+
+
+
+duration = 10.0
+time_elapsed = 0.0
+def move_object(start_pos, end_pos, step_size, prim_path="/World/TargetCube"):
+    global time_elapsed
+    prim = omni.usd.get_context().get_stage().GetPrimAtPath(prim_path)
+    if time_elapsed < duration:
+        time_elapsed += step_size
+        fraction = min(time_elapsed / duration, 1.0)
+        current_pos = start_pos + (end_pos - start_pos) * fraction
+        cpgf = Gf.Vec3d(current_pos[0], current_pos[1], current_pos[2])
+        # Set new position
+        xform = UsdGeom.Xformable(prim)
+        # xform.AddTranslateOp().Set(current_pos)
+        print("times", time_elapsed, duration)
+        print("\tstep_size", step_size)
+        print("\tsetting current pos", current_pos, cpgf)
+        print("\tpositions", start_pos, end_pos)
+        prim.GetAttribute("xformOp:translate").Set(cpgf)
+
+
+
+
+def set_prim_visibility_attribute(prim_path: str, value: str):
+    """Set the prim visibility attribute at prim_path to value
+
+    Args:
+        prim_path (str, required): The path of the prim to modify
+        value (str, required): The value of the visibility attribute
+    """
+    # You can reference attributes using the path syntax by appending the
+    # attribute name with a leading `.`
+    prop_path = f"{prim_path}.visibility"
+    omni.kit.commands.execute(
+        "ChangeProperty", prop_path=Sdf.Path(prop_path), value=value, prev=None
+    )
+
+
+def hide_prim(prim_path: str):
+    """Hide a prim
+
+    Args:
+        prim_path (str, required): The prim path of the prim to hide
+    """
+    set_prim_visibility_attribute(prim_path, "invisible")
+
+
+from isaacsim.robot.manipulators.examples.franka import KinematicsSolver
+
+import typing
+
+def get_world_transform_xform(prim: Usd.Prim) -> typing.Tuple[Gf.Vec3d, Gf.Rotation, Gf.Vec3d]:
+    """
+    Get the local transformation of a prim using omni.usd.get_world_transform_matrix().
+    See https://docs.omniverse.nvidia.com/kit/docs/omni.usd/latest/omni.usd/omni.usd.get_world_transform_matrix.html
+    Args:
+        prim: The prim to calculate the world transformation.
+    Returns:
+        A tuple of:
+        - Translation vector.
+        - Rotation quaternion, i.e. 3d vector plus angle.
+        - Scale vector.
+    """
+    world_transform: Gf.Matrix4d = omni.usd.get_world_transform_matrix(prim)
+    translation: Gf.Vec3d = world_transform.ExtractTranslation()
+    rotation: Gf.Rotation = world_transform.ExtractRotation()
+    scale: Gf.Vec3d = Gf.Vec3d(*(v.GetLength() for v in world_transform.ExtractRotationMatrix()))
+    return translation, rotation, scale
+
+
 
 class HelloWorld(BaseSample):
     def __init__(self) -> None:
@@ -77,39 +167,58 @@ class HelloWorld(BaseSample):
         # )
         # robot_prim = self.world.scene.stage.OverridePrim(prim_path)
         # robot_prim.GetReferences().AddReference(dest_path)
-        print(omni.usd.get_world_transform_matrix(
-            omni.usd.get_context().get_stage().GetPrimAtPath(prim_path)
-        ))
+        # print(omni.usd.get_world_transform_matrix(
+        #     omni.usd.get_context().get_stage().GetPrimAtPath(prim_path)
+        # ))
 
 
+        port_prim = omni.usd.get_context().get_stage().GetPrimAtPath(PORT_PRIM_PATH)
+        # print("Port prim coming up")
+        # print(port_prim)
+        # print(port_prim is None)
+        port_world_position = get_world_transform_xform(port_prim)[0]
+        # print(port_world_position) # (-7.750999993085862, -109.62536725783971, 178.21793722812936)
+        # offset = np.array([1.75, 0, 2])
+        offset = np.array([0, 0, -5])
+        # np.array([-6, -110, 180])
         # Initialize a predefined task for the robot (e.g., following a target)
         my_task = FollowTarget(
             name="follow_target_task",
             franka_prim_path=prim_path,  # Path to the robot's prim in the scene
             franka_robot_name="fancy_franka",  # Name for the robot instance
             target_name="target",  # Name of the target object the robot should follow
-            target_position=np.array([-6, -110, 180]),
+            # target_prim_path="/World/TargetCube",
+            target_position=np.array(port_world_position) + offset,
         )
-
         # Add the task to the simulation world
         world.add_task(my_task)
+
         return
 
     async def setup_post_load(self):
         # Set up post-load configurations, such as controllers
         self._world = self.get_world()
         self._franka = self._world.scene.get_object("fancy_franka")
+
+        # hide_prim("/World/TargetCube")
+        targetCube = omni.usd.get_context().get_stage().GetPrimAtPath("/World/TargetCube")
+        # assert targetCube.GetAttribute("visibility").Get() == "invisible"
+
+
         # Set Position (x, y, z) and Rotation (quaternion)
-        new_position = np.array([34.0, -100.0, 140.0])
+        new_position = np.array([34.0 - 4.0, -100.0 + 10.0, 140.0 + 10.0])
         new_orientation = np.array([1.0, 0.0, 0.0, 0.0]) # w, x, y, z
         Articulation("/panda").set_world_poses(positions=new_position, orientations=new_orientation)
-        print(Articulation("/panda").get_world_poses())
+        # print(Articulation("/panda").get_world_poses())
 
         # Initialize the RMPFlow controller for the robot
         self._controller = RMPFlowController(
             name="target_follower_controller",
             robot_articulation=self._franka
         )
+        # self._controller = KinematicsSolver(
+        #     self._franka
+        # )
 
         # Add a physics callback for simulation steps
         self._world.add_physics_callback("sim_step", callback_fn=self.physics_step)
@@ -129,6 +238,19 @@ class HelloWorld(BaseSample):
         # Perform a simulation step and compute actions for the robot
         world = self.get_world()
         observations = world.get_observations()
+        # print("phys step orientation", observations["target"]["orientation"]) # np.array([0, 0, 1, 0])
+
+        # Move target
+        port_prim = omni.usd.get_context().get_stage().GetPrimAtPath(PORT_PRIM_PATH)
+        port_world_position = get_world_transform_xform(port_prim)[0]
+        start_pos = port_world_position + np.array([0, 0, -5])
+
+        port_prim = omni.usd.get_context().get_stage().GetPrimAtPath(PORT_PRIM_PATH_2)
+        port_world_position = get_world_transform_xform(port_prim)[0]
+        end_pos = port_world_position + np.array([0, 0, 5])
+        print("start vs end\n", "\t", start_pos, "\n\t", end_pos)
+
+        move_object(start_pos, end_pos, step_size)
 
         # Compute actions for the robot to follow the target's position and orientation
         actions = self._controller.forward(
@@ -139,6 +261,17 @@ class HelloWorld(BaseSample):
         # Apply the computed actions to the robot
         self._franka.apply_action(actions)
         return
+
+        # actions, succ = self._controller.compute_inverse_kinematics(
+        #     target_position=observations["target"]["position"],
+        #     target_orientation=observations["target"]["orientation"],
+        # )
+        # if succ:
+        #     self._franka.get_articulation_controller.apply_action(actions)
+        # else:
+        #     carb.log_warn("IK did not converge to a solution.  No action is being taken.")
+
+        # return
 
     def world_cleanup(self):
         return
